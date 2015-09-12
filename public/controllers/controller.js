04fe60c7c3ebc50a4f48eb02298febb1c9f7d4c3
slanto <slanto@slanto.com>
2015-09-12 16:40:23 +0200

    Add month and year

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,48 +1,91 @@
-var myApp = angular.module('myApp', []);
+(function () {
+	'use strict';
 
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-var refresh = function() {
-  $http.get('/contactlist').success(function(response) {
-    $scope.contactlist = response;
-    $scope.contact = "";
-  });
-};
+  var myApp = angular.module('myApp', []);
 
-var init = function() {
-  refresh();
-};
+	  myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+			var locale = "pl-PL";
 
-init();
+			var refresh = function() {
+				$http.get('/contactlist').success(function(response) {
+					$scope.contactlist = response;
+					clearAndSetDefault();
+				});
+			};
 
-$scope.addContact = function(){
-  $http.post('/contactlist', $scope.contact).success(function(response) {
-    console.log(response);
-    refresh();
-  });
-};
+			var init = function() {
+				$scope.years = getYears();
+				$scope.months = getMonths();
+				refresh();
+			};
 
-$scope.delete = function(id) {
-  console.log(id);
-  $http.delete('/contactlist/' + id).success(function(response) {
-    console.log(response);
-    refresh();
-  });
-};
+			var clearAndSetDefault = function() {
+				$scope.contact = {
+					year: new Date().getFullYear(),
+					month: { id: new Date().getMonth() + 1 },
+					created: new Date()
+				};
+			};
 
-$scope.edit = function(id){
-  console.log(id);
-  $http.get('/contactlist/' + id).success(function(response) {
-    console.log(response);
-    $scope.contact = response;
-  });
-};
+			init();
 
-$scope.update = function() {
-  console.log($scope.contact._id);
-  $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
-    console.log(response);
-    refresh();
-  });
-};
+			$scope.clear = function() {
+				clearAndSetDefault();
+			};
 
-}]);
+			$scope.addContact = function(){
+				$http.post('/contactlist', $scope.contact).success(function(response) {
+					refresh();
+				});
+			};
+
+			$scope.delete = function(id) {
+				$http.delete('/contactlist/' + id).success(function(response) {
+					refresh();
+				});
+			};
+
+			$scope.edit = function(id){
+				console.log(id);
+				$http.get('/contactlist/' + id).success(function(response) {
+					console.log(response);
+					$scope.contact = response;
+				});
+			};
+
+			$scope.update = function() {
+				$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+					refresh();
+				});
+			};
+
+		 //TODO: move it to server.js
+		 function getYears() {
+			var currentYear = new Date().getFullYear();
+			var years = [];
+			var startYear = 2009
+			while (startYear <= currentYear) {
+				years.push(startYear);
+				startYear++;
+			}
+			return years;
+		};
+
+		//TODO: move it to server.js
+		function getMonths() {
+			//TODO: try http://momentjs.com/
+			var months = [];
+			for(var i = 1; i <= 9; i++) {
+				var date = new Date("2015-0" + i + "-01");
+				months.push({ id: i, name: date.toLocaleString(locale, { month: "long" })});
+			}
+			for(var i = 10; i <= 12; i++) {
+				var date = new Date("2015-" + i + "-01");
+				months.push({ id: i, name: date.toLocaleString(locale, { month: "long" })});
+			}
+			return months;
+		};
+
+	}]);
+
+}());
--- server.js
+++ server.js
@@ -8,17 +8,13 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
 app.get('/contactlist', function(req, res) {
-  console.log("I received a GET request");
-
   db.contactlist.find(function(err, docs) {
-    console.log(docs);
     res.json(docs);
   });
 
 });
 
 app.post('/contactlist', function(req, res){
-  console.log(req.body);
   db.contactlist.insert(req.body, function(err, doc) {
     res.json(doc);
   });
@@ -26,7 +22,6 @@ app.post('/contactlist', function(req, res){
 
 app.delete('/contactlist/:id', function(req, res) {
   var id = req.params.id;
-  console.log(id);
   db.contactlist.remove({ _id: mongojs.ObjectId(id) }, function(err, doc) {
     res.json(doc);
   });
@@ -43,7 +38,14 @@ app.put('/contactlist/:id', function(req, res) {
   var id = req.params.id;
   db.contactlist.findAndModify({
       query: {_id: mongojs.ObjectId(id)},
-      update: { $set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+      update: { $set: {
+        name: req.body.name,
+        email: req.body.email,
+        number: req.body.number,
+        year: req.body.year,
+        month: req.body.month,
+        created: req.body.created
+      }},
       new: true
   }, function(err, doc) {
     res.json(doc);
